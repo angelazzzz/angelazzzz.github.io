94d4ce3d03352967bdc1af3074e89f0079994b72
Angela Z <angelazzz.uwu@gmail.com>
2022-12-04 04:54:08 -0800

    added reply ui & ux, set up UUID

--- index.js
+++ index.js
@@ -14,27 +14,57 @@ const firebaseConfig = {
 
   var database=firebase.database()
 
-  /*function saveAndUpdate(thisIsACategory){
-    save(thisIsACategory);
-    newMessage(currentResponse);
+  function setUserID(){
+    if(localStorage.getItem('user')===null)
+      {let userID=uuidv4();
+        localStorage.setItem('user', userID);
+      }
   }
-  */
+
+  function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+  
 
   let currentResponse;
   let messageCountSports = 0;
   function save(thisIsACategory){
 
-    var response = document.getElementById('response').value
-    currentResponse = response;
+    var response = document.getElementById('usermessage').value;
 
-    let messageID = ' ' + new Date().getTime();
+    let messageID = new Date().getTime();
+    let user = localStorage.getItem('user');
 
     database.ref('palisade/' + thisIsACategory+'/' + messageID).set({
-      messageID: messageID,
       message: response,
-      user: 1
+      user: user
     })
     render(thisIsACategory);
+  }
+
+  function saveReply(thisIsACategory, messageID){
+
+    var reply = document.getElementById(messageID).value;
+
+    let replyID = new Date().getTime();
+
+    let user;
+
+    /*let responseReference = database.ref('palisade/' + thisIsACategory + '/' + messageID)
+    responseReference.on('value', function(snapshot){
+      let childSnapVal = childSnapshot.val();
+      user = childSnapVal.user;
+    });*/
+
+    //if(localStorage.getItem('user')!==user){
+      database.ref('palisade/' + thisIsACategory+'/' + messageID + '/replies/' + replyID).set({
+        message: reply,
+        //user: user
+      })
+    //}
+    //console.log('hi');
   }
 
   //viewing/rendering the user-responses
@@ -47,13 +77,41 @@ const firebaseConfig = {
       snapshot.forEach(function(childSnapshot) {
         let childSnapVal = childSnapshot.val();
         let childMessageID = childSnapshot.key;
-
         let childMessage = childSnapVal.message;
-        let element = document.createElement('p');
-        element.innerText = childMessage;
-        element.id = childMessageID;
-        element.className = "response";
-        userResponses.appendChild(element);
+
+        // NOTE: the response section is created for each element here
+        let responseDiv = document.createElement('div');
+        responseDiv.className = "response-container";
+
+        let responseMsg = document.createElement('p');
+        responseMsg.innerText = childMessage;
+        responseMsg.className = "response-msg";
+
+        let responseRepliesDiv = document.createElement('div');
+        responseRepliesDiv.className = "response-replies-div";
+
+        let responseReplyButton = document.createElement('button');
+        responseReplyButton.addEventListener("click", function saveIt(){
+          saveReply('sports', childMessageID);
+        });
+        responseReplyButton.className = "response-reply-button";
+
+        let responseReplyButtonImg = document.createElement('img');
+        responseReplyButtonImg.src = "reply-icon.png"
+        responseReplyButtonImg.className = "response-reply-button-img";
+
+        let responseReplyInput = document.createElement('input');
+        responseReplyInput.id = childMessageID;
+        responseReplyInput.type = 'text';
+        responseReplyInput.placeholder = "Enter your response";
+        responseReplyInput.className = "response-reply-input";
+
+        responseDiv.appendChild(responseMsg);
+        responseReplyButton.appendChild(responseReplyButtonImg);
+        responseRepliesDiv.appendChild(responseReplyButton);
+        responseRepliesDiv.appendChild(responseReplyInput);
+        responseDiv.appendChild(responseRepliesDiv);
+        userResponses.appendChild(responseDiv);
       });
     });
   }
